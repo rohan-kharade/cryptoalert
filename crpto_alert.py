dda1833e4495507babae81d29b82161e1762e5f1
rohankharade <kharade.rohan@yahoo.com>
2018-01-17 23:29:30 +0530

    API call correction

--- crpto_alert.py
+++ crpto_alert.py
@@ -4,37 +4,39 @@ import urllib.request
 import urllib.parse
 import time
 
+# check_price function is used to get price from different trading platform APIs
+# def select_coin(): // currency selection to get notified
 
-def check_Bitcoin_price_koinex():
-	Koinex_URL = 'https://koinex.in/api/ticker'
-	r = requests.get(Koinex_URL)
+def check_price_BTC_ETC_koinex():
+	Koinex_api = 'https://koinex.in/api/ticker'
+	# Zebpay_api, btcx_api, ethex_api
+	r = requests.get(Koinex_api)
 	j = r.json()
-	return j['prices']['BTC'],j['prices']['ETH']
-	#return j['sell'],j['buy']
-		
+	return j['prices']['BTC'],j['prices']['ETH'] # j['prices']['selected_coins']
+
+# send_SMS function is used to send trading alert via SMS using textlocal's SMS gateway API
+
 def sendSMS(apikey, numbers, sender, message):
     data =  urllib.parse.urlencode({'apikey': apikey, 'numbers': numbers,
         'message' : message, 'sender': sender})
     data = data.encode('utf-8')
-    request = urllib.request.Request("https://api.txtlocal.com/send/?")
+    request = urllib.request.Request("https://api.textlocal.in/send/?")
     f = urllib.request.urlopen(request, data)
     fr = f.read()
     return(fr)
 
 
-#while (Bitcoin_Sell < 900000.0):
-while (1):
-	Bitcoin_Sell, ETH_Sell = check_Bitcoin_price_koinex()
+while (Bitcoin_Sell < 140000.0):
+	Bitcoin_Sell, ETH_Sell = check_price_Bitcoin_koinex()
 	print ("Bitcoin Sell :-",Bitcoin_Sell,"INR")
 	print ("ETH Sell     :-",ETH_Sell,"INR")
 	time.sleep (2)
 
+resp =  sendSMS('bAZAwXcHereYourSMSapikeyBmSBy7512njuIAwR1chB676', '919689164280',
+    'you@email.com', Bitcoin_Sell)
+print (resp)
 
-#resp =  sendSMS('bAZAwXcCoik-Y2NCBTXBmSBy7512njuIAwR1chB676', '919689164280',
- #   'support@wibronet.com', Bitcoin_Sell)
-#print (resp)
-
-	
+# learn_trade(): function to understand progress curve of trading and notify subscriber accordingly	
 
 
 
